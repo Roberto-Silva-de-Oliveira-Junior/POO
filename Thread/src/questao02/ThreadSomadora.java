c786692d6e2ba030dc8e1064f834ae6960caa439
Roberto Silva de Oliveira Júnior <roberto.silva@dce.ufpb.br>
2017-06-04 13:49:39 -0300

    Entrega de atividades

--- Thread/src/questao02/ThreadSomadora.java
+++ Thread/src/questao02/ThreadSomadora.java
@@ -4,23 +4,23 @@ public class ThreadSomadora implements Runnable {
 	private int inicio;
 	private int fim;
 	private int[][] num;
-	int soma = 0;
+	static int soma;
 
 	public ThreadSomadora(int inicio, int fim, int[][] num) {
 		this.inicio = inicio;
 		this.fim = fim;
 		this.num = num;
+		soma=0;
 	}
-	public   synchronized int getSoma(){
+	public int getSoma(){
 		return this.soma;
 	}
-
 	@Override
 	public void run() {
 		for (int k = this.inicio; k < fim; k++) {
 			for (int i = this.inicio; i < fim; i++) {
 				if(num[k][i]!=0){
-					soma++;
+					soma+=num[k][i];
 				}
 			}
 		}
